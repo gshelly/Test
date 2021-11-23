be859b0ef9d732622ab5f0a31d91f926b5c2e52a
gshelly <gshelly1402@gmail.com>
2021-11-23 15:03:11 -0800

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Shelly Gupta on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
